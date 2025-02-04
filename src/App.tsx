ef31f8188d53dfe3093936376ff815ba96706c14
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2025-02-04 12:14:24 +0530

    App init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return <div className="flex justify-center items-center h-screen">
+    <Button>Click me</Button>
+  </div>;
+};
+
+export default App;
