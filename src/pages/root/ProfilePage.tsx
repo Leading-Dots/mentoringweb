34f8746754fa1109bcff6154a9395c0705a61b24
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2025-03-28 18:33:22 +0530

    Fixes #189

--- src/pages/root/ProfilePage.tsx
+++ src/pages/root/ProfilePage.tsx
@@ -2,10 +2,12 @@ import { ProfileStatus } from "@/API";
 import { ProfileForm } from "@/components/profile/ProfileForm";
 import { useAuth } from "@/hooks/useAuth";
 import { transformNullValues } from "@/lib/utils";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
+import { AlertCircle } from "lucide-react";
 const ProfilePage = () => {
-  const { user } = useAuth();
-
-
+  const { user, refreshUser } = useAuth();
 
   const isProfilePublished = user.profileStatus === ProfileStatus.PUBLISHED;
 
@@ -13,14 +15,34 @@ const ProfilePage = () => {
 
   console.log("user", user);
 
-
-
   const transformedData = transformNullValues(user, user?.role);
 
-  
   return (
     <div className="flex flex-col max-w-2xl gap-2 p-4">
-      <ProfileForm role={user?.role} isProfilePublished={isProfilePublished} initialData={transformedData} />
+      {user!.profileStatus === ProfileStatus.INPROGRESS && (
+        <Alert variant="default" className="mb-4 ">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Profile Under Review</AlertTitle>
+          <AlertDescription className="flex items-center justify-between">
+            Your profile is currently being reviewed by admin.
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => {
+                refreshUser();
+              }}
+            >
+              <RefreshCcw className="h-4 w-4" />
+              Check Status
+            </Button>
+          </AlertDescription>
+        </Alert>
+      )}
+      <ProfileForm
+        role={user?.role}
+        isProfilePublished={isProfilePublished}
+        initialData={transformedData}
+      />
     </div>
   );
 };
